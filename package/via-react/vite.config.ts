35f2865201dfb7a57727255edc30224f85438c9f
designDefined <designdefined.by@gmail.com>
2024-10-10 15:11:35 +0900

    feat: Setup via-react package

---
+++ package/via-react/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ include: ["index.ts", "src"] })],
+  build: {
+    lib: {
+      name: "index",
+      formats: ["es", "cjs"],
+      fileName: (module, entry) => `${entry}.${module === "es" ? "m" : "c"}js`,
+      entry: resolve(__dirname, "index.ts"),
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+});
